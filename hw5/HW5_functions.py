a0cfe732fca90caead14f9134a70ecebd31d6dfb
Ryan Mitchell <rmitchell@HSE-BUL-MAC-08.fios-router.home>
2017-10-15 23:58:09 -0400

    Adding hw5

---
+++ hw5/HW5_functions.py
@@ -0,0 +1,35 @@
+import numpy as np
+#--------  visualize_prob
+# A function to visualize the probabilities predicted by a Logistic Regression model
+# Input: 
+#      model (Logistic regression model)
+#      x (n x d array of predictors in training data)
+#      y (n x 1 array of response variable vals in training data: 0 or 1)
+#      ax (an axis object to generate the plot)
+
+def visualize_prob(model, x, y, ax):
+    print(np.mean([1,23,4,5]))
+    # Use the model to predict probabilities for
+    y_pred = model.predict_proba(x)
+    
+    # Separate the predictions on the label 1 and label 0 points
+    ypos = y_pred[y==1]
+    yneg = y_pred[y==0]
+    
+    # Count the number of label 1 and label 0 points
+    npos = ypos.shape[0]
+    nneg = yneg.shape[0]
+    
+    # Plot the probabilities on a vertical line at x = 0, 
+    # with the positive points in blue and negative points in red
+    pos_handle = ax.plot(np.zeros((npos,1)), ypos[:,1], 'bx', label = 'ALL')
+    neg_handle = ax.plot(np.zeros((nneg,1)), yneg[:,1], 'rx', label = 'AML')
+    
+    # Line to mark prob 0.5
+    ax.axhline(y = 0.5, color = 'k', linestyle = '--')
+    
+    # Add y-label and legend, do not display x-axis, set y-axis limit
+    ax.set_ylabel('Probability of AML class')
+    ax.legend(loc = 'best')
+    ax.get_xaxis().set_visible(False)
+    ax.set_ylim([0,1])
