c6a7e764166c8dcac3c12f635556ad496c85c715
rmitchell <ryan.e.mitchell@gmail.com>
2017-11-28 23:24:49 -0500

    Adding views

--- final/artist.py
+++ final/artist.py
@@ -3,11 +3,27 @@ from pymysql.err import InternalError
 
 class Artist:
     def __init__(self, spotifyId, name, genres, followers, popularity):
+        self.id = None
         self.spotifyId = spotifyId
         self.name = name.encode('utf-8')
         self.genres = genres
         self.followers = followers
         self.popularity = popularity
+        self.views = None
+    
+    def fromDB(row):
+        artist = Artist(row['spotifyId'], row['name'], row['genres'].split(','), row['followers'], row['popularity'])
+        artist.id = row['id']
+        return artist
+
+    def getAllWithoutViews(db):
+        db.cur.execute("SELECT * FROM artists WHERE views=0")
+        rows = db.cur.fetchall()
+        return [Artist.fromDB(row) for row in rows]
+
+    def updateViews(self, db):
+        db.cur.execute("UPDATE artists SET views = %s WHERE id = %s", (self.views, self.id))
+        db.conn.commit()
 
     def loadBySpotifyId(db, spotifyId):
         if not spotifyId:
